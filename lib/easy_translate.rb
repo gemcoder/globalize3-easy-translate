fe7396a29b57ecd0a4238b810cf087d742965229
pawel <pawel@pawel-desktop.(none)>
2010-10-11 15:37:23 +0100

    globalize3-easy-translate based on globalize2-easy-translate by http://github.com/bsamman

--- lib/easy_translate.rb
+++ lib/easy_translate.rb
@@ -1,17 +1,26 @@
 module EasyTranslate
-	def self.included(base)
-		base.extend InstanceMethods
-	end
-	module InstanceMethods
-			def easy_translate options = {}
-				options[:locales].each do |this_locale|
-					globalize_options[:translated_attributes].each do |this_method|
-						class_eval(%|def #{this_locale}_#{this_method}; globalize.fetch("#{this_locale}","#{this_method}"); end;|)
-						class_eval("def #{this_locale}_#{this_method}=(this_value);" +
-						%|globalize.stash("#{this_locale}","#{this_method}", this_value); end;|)
-					end unless globalize_options[:translated_attributes].empty?
-				end if options[:locales]
-				
-			end	
-		end	
+  def self.included(base)
+    base.send :extend, ClassMethods
+  end
+  
+  module ClassMethods
+    def easy_translate( options = {})
+      if !options.empty?
+        begin
+          if options.length == 0
+            options = {:locales => I18n.available_locales}
+          end
+        rescue
+        options = {}
+        end
+      end
+      
+      options[:locales].each do |this_locale|
+        translated_attribute_names.each do |this_method|
+          class_eval(%|def #{this_locale}_#{this_method}; globalize.fetch("#{this_locale}","#{this_method}"); end;|)
+          class_eval("def #{this_locale}_#{this_method}=(this_value);" + %|globalize.write("#{this_locale}","#{this_method}", this_value); end;|)
+        end unless translated_attribute_names.empty?
+      end if options[:locales]
+    end
+  end  
 end
